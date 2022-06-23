5c59ca979f997e9790d6debfdb0a358ad312b586
luiz-henrique112 <108096881+luiz-henrique112@users.noreply.github.com>
2022-06-23 17:50:13 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+
+if (keyIsDown(RIGHT_ARROW)) {
+  
+box.position.x = box.position.x +1
+
+}
+
+  drawSprites()
+
+}
+
+
+
+
